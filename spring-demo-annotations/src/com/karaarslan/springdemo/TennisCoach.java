cc111cc04ce20eec82fbfaa9f21c34fcf93e5ee2
Erhan Karaarslan <erhannkaraarslan@gmail.com>
2022-07-19 12:45:38 +0300

    bean lifecycle - @PostConstruct/@PreDestroy

--- spring-demo-annotations/src/com/karaarslan/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/karaarslan/springdemo/TennisCoach.java
@@ -1,12 +1,13 @@
 package com.karaarslan.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -22,6 +23,18 @@ public class TennisCoach implements Coach {
 		System.out.println(">> TennisCoach : inside default constructor");
 	}
 
+	//define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach : inside doMyStartupStuff() method");
+	}
+	//define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach : inside doMyCleanupStuff() method");
+	}
+	
+	
 //	@Autowired
 //	public void setFortuneService(FortuneService fortuneService) {
 //		System.out.println(">> TennisCoach : inside setter injection");
