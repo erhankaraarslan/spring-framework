c03898fd67f9ebed30e7bf9b3ec470632f57d2a3
Erhan Karaarslan <erhannkaraarslan@gmail.com>
2022-07-20 09:47:31 +0300

    Binding @RequestParam

--- spring-mvc-demo/src/com/karaarslan/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/karaarslan/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -25,6 +26,15 @@ public class HelloWorldController {
 		String theName=request.getParameter("studentName");
 		theName=theName.toUpperCase();
 		String result="Hey! "+theName;
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model)
+	{
+		theName=theName.toUpperCase();
+		String result="Hey my friend from v3! "+theName;
 		model.addAttribute("message", result);
 		return "helloworld";
 	}
