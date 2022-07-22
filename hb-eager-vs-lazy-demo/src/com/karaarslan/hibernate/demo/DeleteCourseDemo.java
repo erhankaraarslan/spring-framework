79410f2667dd143fb15c0810ab510ed4991bd709
Erhan Karaarslan <erhannkaraarslan@gmail.com>
2022-07-22 09:48:51 +0300

    Eager vs Lazy Loading

---
+++ hb-eager-vs-lazy-demo/src/com/karaarslan/hibernate/demo/DeleteCourseDemo.java
@@ -0,0 +1,41 @@
+package com.karaarslan.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.karaarslan.hibernate.demo.entity.Course;
+import com.karaarslan.hibernate.demo.entity.Instructor;
+import com.karaarslan.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteCourseDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start transaction
+			session.beginTransaction();
+
+			int theId = 10;
+
+			Course course = session.get(Course.class, theId);
+			session.delete(course);
+
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
