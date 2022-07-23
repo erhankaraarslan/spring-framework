d0e01f3d0807c5f1329c4b00e585e0e21d2fdadb
Erhan Karaarslan <erhannkaraarslan@gmail.com>
2022-07-23 21:26:58 +0300

    AOP: Ordering Aspects

---
+++ spring-demo-aop-pointcut-order-aspects/src/com/karaarslan/aopdemo/MainDemoApp.java
@@ -0,0 +1,35 @@
+package com.karaarslan.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.karaarslan.aopdemo.dao.AccountDAO;
+import com.karaarslan.aopdemo.dao.MemberShipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		MemberShipDAO theMemberShipDAO = context.getBean("memberShipDAO", MemberShipDAO.class);
+
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		theAccountDAO.setName("A");
+		theAccountDAO.setServiceCode("B");
+		theAccountDAO.getName();
+		theAccountDAO.getServiceCode();
+
+		// call the theMemberShipDAO.addAccount
+		theMemberShipDAO.addSillyMember();
+		theMemberShipDAO.goToSleep();
+		// close the context
+		context.close();
+	}
+
+}
