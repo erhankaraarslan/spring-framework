77f05109bb0879596dbd88a730f55b38200d0a33
Erhan Karaarslan <erhannkaraarslan@gmail.com>
2022-07-24 12:43:52 +0300

    AOP: @Around Advice

---
+++ spring-demo-aop-zz-around/src/com/karaarslan/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.karaarslan.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.karaarslan.aopdemo")
+public class DemoConfig {
+
+}
