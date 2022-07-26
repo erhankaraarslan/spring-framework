c1a8c170cc33aa47a9ce394a01ee1fcb75a7caa1
Erhan Karaarslan <erhannkaraarslan@gmail.com>
2022-07-26 12:11:23 +0300

    load the student data ... only once! with @PostConstruct

--- spring-rest-demo/src/main/java/com/karaarslan/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/karaarslan/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.karaarslan.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +15,21 @@ import com.karaarslan.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
+	private List<Student> theStudents;
+
+	// define @PostConstruct to load the student data ... only once!
+
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
+		theStudents.add(new Student("Erhan1", "Karaarslan"));
+		theStudents.add(new Student("Merve2", "Karaarslan"));
+		theStudents.add(new Student("Efe Hakan", "Yüksel"));
+
+	}
+
 	@GetMapping("/students")
 	public List<Student> getStudent() {
-		List<Student> students = new ArrayList<>();
-
-		students.add(new Student("Erhan", "Karaarslan"));
-		students.add(new Student("Merve", "Karaarslan"));
-		students.add(new Student("Efe Hakan", "Yüksel"));
-
-		return students;
+		return theStudents;
 	}
 }
